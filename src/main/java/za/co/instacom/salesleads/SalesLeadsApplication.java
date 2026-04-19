da14575beb786ffd7cda250b54e01428777b0a7a
Tonderai Muchada <tonderaimuchada@gmail.com>
2026-04-19 17:22:53 +0200

    Pushing ommitted project setup files

---
+++ src/main/java/za/co/instacom/salesleads/SalesLeadsApplication.java
@@ -0,0 +1,13 @@
+package za.co.instacom.salesleads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesLeadsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesLeadsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/za/co/instacom/salesleads/SalesLeadsApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.instacom.salesleads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesLeadsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
